57cda131680976393db98f9973f3f70033c6b602
KeisukeYamashita <19yamashita15@gmail.com>
2021-10-24 14:42:42 +0900

    Update documents
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/jmatsu/terraform-provider-slack/slack"
 )
 
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: slack.Provider})
